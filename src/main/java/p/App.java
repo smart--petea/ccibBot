f59b52989f66cba55ae71d9a2b6b6d80df6ced55
petru <smart__petea@mail.ru>
2016-09-30 15:12:39 +0300

    getUrl

--- src/main/java/p/App.java
+++ src/main/java/p/App.java
@@ -1,13 +1,30 @@
 package p;
 
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import java.net.URL;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        System.out.println(getUrl("http://www.ccib.ro/ro/CCIB/4/486/452/lista+firme+a.html"));
+    }
+
+    public static String getUrl(String url) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        URL ccib = new URL(url);
+        BufferedReader in = new BufferedReader(new InputStreamReader(ccib.openStream()));
+
+        String line;
+        while((line = in.readLine()) != null) sb.append(line);
+
+        in.close();
+
+        return sb.toString();
     }
 }

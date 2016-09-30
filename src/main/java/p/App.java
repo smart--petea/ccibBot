1e958f6460bb99dbdbe6e7ec70d2302dad730542
petru <smart__petea@mail.ru>
2016-09-30 16:41:06 +0300

    isolate company text

--- src/main/java/p/App.java
+++ src/main/java/p/App.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 import java.net.URL;
+import java.util.regex.*;
 
 /**
  * Hello world!
@@ -12,10 +13,19 @@ import java.net.URL;
 public class App
 {
     public static void main( String[] args ) throws Exception {
-        System.out.println(getUrl("http://www.ccib.ro/ro/CCIB/4/486/452/lista+firme+a.html"));
+        String urlContent = getUrlContent("http://www.ccib.ro/ro/CCIB/4/486/452/lista+firme+a.html");
+        String pattern  = "(<hr />|(?<=<hr />)).*?(<hr />|</div>)";
+        Pattern hrPattern = Pattern.compile(pattern, Pattern.MULTILINE | Pattern.DOTALL);
+        Matcher m = hrPattern.matcher(urlContent);
+
+        while(m.find()) {
+            System.out.println("...");
+            System.out.println("...");
+            System.out.println(urlContent.substring(m.start(), m.end()));
+        }
     }
 
-    public static String getUrl(String url) throws Exception {
+    public static String getUrlContent(String url) throws Exception {
         StringBuilder sb = new StringBuilder();
         URL ccib = new URL(url);
         BufferedReader in = new BufferedReader(new InputStreamReader(ccib.openStream()));
